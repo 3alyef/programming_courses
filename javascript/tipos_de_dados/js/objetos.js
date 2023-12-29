713dc07ab740eb859c0328945c7d6d9f865e8243
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2023-12-29 12:56:17 -0300

    callback class

--- javascript/tipos_de_dados/js/objetos.js
+++ javascript/tipos_de_dados/js/objetos.js
@@ -1,6 +1,22 @@
-const heb = document.getElementById('he')
-heb.textContent = 'hello'
-const par = document.createElement('p')
-heb.appendChild(par)
-var a = document.createTextNode('oi')
-par.appendChild(a)
+
+var p= document.createElement('p')
+var select = document.querySelector('#seletor')
+select.appendChild(p)
+
+
+const exibir = (num)=>{
+    p.innerHTML += `A operação resultou em: ${num}<br>`
+}
+
+const soma = (a, b, callback) => {
+    var op = a+b;
+    callback(op)
+}
+
+const mult = (a, b, cb) =>{
+    var op = a*b;
+    cb(op)
+}
+
+soma(2, 5, exibir)
+mult(2, 5, exibir)
