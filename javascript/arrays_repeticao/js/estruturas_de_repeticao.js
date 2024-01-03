103372240ea69c550864024891ad49bb1a7503e3
Alef A. Araujo <123469430+alefalves4867@users.noreply.github.com>
2024-01-03 14:53:09 -0300

    nova_aula

---
+++ javascript/arrays_repeticao/js/estruturas_de_repeticao.js
@@ -0,0 +1,27 @@
+const names = ["Alef", "Avraham", "Itzkhaq", "Yaqov", "Mosheh", "David", "Shlomo"]
+
+const input = document.createElement('input')
+input.setAttribute('type', 'text')
+input.classList = 'text'
+const btnInput = document.createElement('input')
+btnInput.setAttribute('type', 'button')
+btnInput.setAttribute('value', 'Search')
+
+document.body.appendChild(input)
+document.body.appendChild(btnInput)
+
+
+let btns = btnInput.addEventListener('click', ()=>{
+    const name = document.body.querySelector('.text').value
+    let i = 0
+    let tf = false
+    names.forEach((el)=>{
+        i++
+        if(el.toUpperCase() == name.toUpperCase()){
+            console.log(`Nome encontrado na base de dados`)
+            tf=true
+        }else if(names.length == i && tf==false){
+            console.log('Nome não encontrado na base de dados!')
+        }
+    })
+})
